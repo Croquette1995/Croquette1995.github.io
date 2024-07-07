4589340931967101fc2d4cf62ddc9c614cb0deef
Super_Coco_95 <corentin.chido@protonmail.com>
2024-07-07 11:32:37 +0200

    test en vue de vérifier la responsivité d'un site web réalisé avec bulma

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+  
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+  
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+      
+    });
+  });
+  });
+
+
 window.addEventListener('scroll', scrollBoutonRemonter)
 
 function scrollBoutonRemonter()
@@ -9,18 +31,22 @@ function scrollBoutonRemonter()
   {
     if ((partieScrollee/partieScrollable) * 100 > 35){
       boutonRemonter.style.display = "flex"; 
+
     }
     else{
       boutonRemonter.style.display = "none"; 
+
     } 
   }
   else
   {
     if((partieScrollee/partieScrollable) * 100 > 60){
       boutonRemonter.style.display = "flex"; 
+
     }
     else{
       boutonRemonter.style.display = "none"; 
+
     }
   }
 }
