632d61313a70289a60fecf3e8e854d3412515012
Super_Coco_95 <corentin.chido@protonmail.com>
2024-08-30 15:46:46 +0200

    update site

--- situation.js
+++ situation.js
@@ -1,95 +1,121 @@
-let dataPresentation;
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     fetch('http://localhost:3000/elcapistranovillage')
-//         .then(response => response.json())
-//         .then(data => {
-//             dataPresentation = data;
-            
-//             // Appeler addElement ici, après que dataPresentation soit définie
-//             addElement("h1", "title", "#containerPresentation", data[0].Titre);
-//             for (let i = 1; i < data.length; i++){
-//                 addElement("p", "mt-3", "#containerPresentation", data[i].paragraphe);           
-//             }
-        
-//         })
-//         .catch(error => {
-//             console.error('Erreur lors de la récupération des données:', error);
-//         });
-// });
 
 function addElement(type, classe, emplacement, contenu){
     let element = document.createElement(type);
     element.classList.add(classe); // Correction de la méthode classList
+    if(classe === "title"){
+        element.id =`${contenu.split(" ").join("")}`;
+    }
     element.textContent = `${contenu}`;
     document.querySelector(emplacement).appendChild(element);
 }
 
 
-//   function createCarousel(images) {
-//     const carousel = document.getElementById('carousel');
-//     images.forEach(image => {
-//       const img = document.createElement('img');
-//       img.src = image;
-//       carousel.appendChild(img);
-//     });
-//   }
+
+async function loadGallery(section, carouselContainerId, pathFolderImages){
+    let data = await fetchData();
+    let images = Object.values(data[section].Images);
+    for(let i=0; i< images.length;i++){
+        let element = document.createElement("div");
+        element.classList.add('card', 'mr-2', 'ml-2');
+        element.innerHTML =
+        `
+            <div class="card-image">
+                <figure class="image is-4by3">
+                    <img src="${pathFolderImages}${images[i]}"/>
+                </figure>
+            </div>
+        `
+        document.querySelector(carouselContainerId).appendChild(element);
+    };
+    carousel(carouselContainerId)
+}
+
+async function loadCard(section, carouselContainerId, pathFolderImages){
+    let data = await fetchData();
+    let card = Object.values(data[section]);
+    card.forEach(membre => {
+        let element = document.createElement("div");
+        element.classList.add('card', 'mr-2', 'ml-2', 'is-shadowless');
+        element.innerHTML =
+        `
+        <header class="card-header has-background-white-ter">
+            <p class="card-header-title is-centered">${membre.Titre}</p>
+        </header>
+        <div class="card-image is-flex-grow-1">
+                <figure class="image is-4by3">
+                    <img src="${pathFolderImages}${membre.Image}"/>
+                </figure>
+        </div>
+        <div class="card-content is-flex-grow-1 has-background-white-ter is-flex-wrap-wrap" >
+            <div class="content is-flex-grow-3">${membre.Description}</div>
+        </div>
+        `
+        console.log(membre.Image);
+        console.log(element.innerHTML);
+
+        document.querySelector(carouselContainerId).appendChild(element);
+    });
+    bulmaCarousel.attach(carouselContainerId, {
+        slidesToScroll: 1,
+        slidesToShow: 3,
+        navigation:true
+    });
+
+}
+
+loadCard("Les incontournables :", "#carouselToSee","./Images/Situation/Incontournables/");
+
+async function carousel(idContainer){
+    bulmaCarousel.attach(idContainer, {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        navigation:true
+    });
+}
 
 
-async function fetchDataCapistrano(){
+async function fetchData(){
     const response = await fetch('situation.json');
     const data = await response.json();
     return data;
 }
 
 async function loadDataCapistrano() {
-    const data = await fetchDataCapistrano();
-    console.log(data);
-    console.log(data.elcapistranovillage[0].Titre);
-    addElement("h1", "title", "#containerPresentation", data.elcapistranovillage[0].Titre);
-    for (let i = 1; i < data.elcapistranovillage.length; i++){
-        addElement("p", "mt-3", "#containerPresentation", data.elcapistranovillage[i].paragraphe);           
-    }
-    for (let i = 0; i < data.nerja.length; i++){
-        addElement("p", "mt-3", "#nerja", data.nerja[i].paragraphe);           
-    }
-}
+    const data = await fetchData();
+    let section = Object.values(data["elcapistranovillage"]);
+    addElement("h1", "title", "#containerPresentation", section[0]);
 
+    section[1].forEach(member => {
+        addElement("p", "mt-3", "#containerPresentation", member);
+    });
+};
 
+async function loadDataNerja() {
+    const data = await fetchData();
+    let section = Object.values(data["nerja"]);
+    addElement("h1", "title", "#nerja", section[0]);
 
+    section[1].forEach(member => {
+        addElement("p", "mt-3", "#nerja", member);
+    });
+};
 
+    
+    
 
 loadDataCapistrano();
+loadDataNerja();
+loadGallery("elcapistranovillage", "#carousel-demo", "./Images/Situation/CapistranoVillage/");
+loadGallery("nerja", "#carouselNerja", "./Images/Situation/Nerja/");
 
-async function fetchImages() {
-    const response = await fetch('images.json');
-    const imageFiles = await response.json();
-    return imageFiles;
-  }
+
+
+
+
   
 
-  async function loadImages() {
-    const images = await fetchImages();
-    console.log(images)
+  
 
-    let nextButton = document.querySelector("#nextButton");
-  let index = 0;
-  document.querySelector("#imageCard").src = `./Images/Situation/CapistranoVillage/${images[index]}`
-
-  nextButton.addEventListener("click", function(){
-    index++;
-    document.querySelector("#imageCard").src = `./Images/Situation/CapistranoVillage/${images[index]}`;
-    if (index === images.length -1){
-        this.style.pointerEvents = "none";
-        this.style.cursor = "default";
-        this.style.backgroundColor= "red";
-        console.log("click");
-      }
-      
-  })
-  }
-
-  loadImages();
 
   let buttonMenu = document.querySelector("#buttonMenu");
 let colMenu = document.querySelector("#colMenu");
