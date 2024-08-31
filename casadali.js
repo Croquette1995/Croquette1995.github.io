619ccf49a46551fe47112adbfbea42e8d763aafa
Super_Coco_95 <corentin.chido@protonmail.com>
2024-08-31 13:50:39 +0200

    update site

--- casadali.js
+++ casadali.js
@@ -38,47 +38,88 @@ async function loadGallery(){
 
 loadGallery();
 
+async function loadGallery1(section){
+    let data = await fetchData();
+    let valeur = Object.values(data[section]["Images"]);
+    console.log(valeur);
+    let col = document.querySelector("#section");
+    let element = document.createElement("div");
+    element.classList.add("column", "carousel");   
+    element.id="galerie";
+    col.appendChild(element);
+
+    let gallery = document.querySelector("#galerie");
+
+    for(let i=0; i< valeur.length;i++){
+        let element = document.createElement("div");
+        element.classList.add("card", "mr-2", "ml-2");
+         
+        element.id="remove";
+        element.innerHTML =`
+                <div class="card-image" id="remove" >
+                    <figure class="image is-3by2">
+                        <img src="./Images/Casadali/${valeur[i]}"/>
+                    </figure>
+                </div>
+        `
+        gallery.appendChild(element);
+    };
+    await bulmaCarousel.attach('#galerie', {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        navigation:true
+    });
+
+    
+}
+
+
+
+
+
 async function loadData(){
     let data = await fetchData();
     
     const value = Object.values(data);
-    console.log(value[0].Images);
-    const keys = Object.keys(data);
-    const entries = Object.entries(data);
-    entries.forEach((key, value) => {
-        console.log(key);
-        console.log(value);
-    });
-
     
+    console.log(value);
+    const keys = Object.keys(data);
+    
+    const entries = Object.entries(data);
 
     addMember(entries);
     document.querySelector(`#${keys[0]}`).classList.add("is-active");
     addDescription(value,0);
+    loadGallery1("Généralités");
 
-    let actif = 'is-active';
-    keys.forEach(nomId => {
-        const element = document.querySelector(`#${nomId.replace(/ /g, '\\ ')}`);
+
+    let liste = document.querySelectorAll("div.tabs > ul > li");
+    liste.forEach(element => {
         element.addEventListener("click", function(){
-            document.querySelector("li.is-active").classList.remove("is-active");
-            document.querySelector(`#${nomId.replace(/ /g, '\\ ')}`).classList.add("is-active")
-            let elementToDelete = document.querySelectorAll("#remove");
-            elementToDelete.forEach(element => {
-            element.remove();
-            });
-            
-
-            entries.forEach((key, valuee) => {
-                if(key[0] === nomId){
-                    addDescription(value, valuee);
-                    
-                        
-                }
-            });
-
-        });
         
-      });
+            let activeElement = document.querySelector("li.is-active").classList.toggle("is-active");
+            element.classList.toggle("is-active");
+            let elementToDelete = document.querySelectorAll("#remove");
+            console.log(elementToDelete)
+            elementToDelete.forEach(element => {
+                element.remove();
+            });
+            let description = data[element.id]['Description'];
+            description.forEach(paragraphe => {
+                let element = document.createElement("li");
+                element.classList.add("mt-3");
+                element.style.textIndent= "5px";
+                element.id="remove";
+                element.textContent=paragraphe;
+                document.querySelector("#description").appendChild(element);
+            });
+            document.querySelector("#section").lastChild.remove();
+            loadGallery1(element.id);
+
+
+        })
+    });
+
     
 }
 
@@ -116,57 +157,22 @@ function addDescription(value, chiffre){
         element.textContent=member;
         document.querySelector("#description").appendChild(element);
     });
-    let galerie = value[chiffre].Images;
-    console.log(galerie);
-    document.querySelector("#image").src=galerie[0];
-    
-
-
-    
-    
-
-
-
-    let buttonRight = document.querySelector("#buttonRight");
-    let buttonLeft = document.querySelector("#buttonLeft");
-    buttonLeft.disabled = true;
-    buttonLeft.style.visibility="hidden";
-    let index=0;
-
     
     
     
-    buttonRight.addEventListener("click", function(){
-        index++;
-        document.querySelector("#image").src=galerie[index];
 
-        if(index === galerie.length -1){
-            buttonRight.disabled = true;
-            buttonRight.style.visibility="hidden";
-        }
-        
-        if(index > 0){
-            buttonLeft.disabled = false;
-            buttonLeft.style.visibility="";
-        }
-    })
-
-    buttonLeft.addEventListener("click", function(){
-        index--;
-        document.querySelector("#image").src=galerie[index];
-
-        if(index < galerie.length -1){
-            buttonRight.disabled = false;
-            buttonRight.style.visibility="";
-        }
-
-        if(index === 0){
-            buttonLeft.disabled = true;
-            buttonLeft.style.visibility="hidden";
-        }
-        
-    })
 }
+    
+    
+
+
+
+    
+
+    
+    
+    
+   
 
 
     
@@ -198,3 +204,4 @@ buttonMenu.addEventListener("click", function(){
         
     }
 })
+
